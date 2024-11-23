6d1c02deb398c5249f6cd8c8cfd8bff905c06627
Joosep Madar <146342736+joosep5@users.noreply.github.com>
2024-11-23 12:22:55 +0200

    add logo
    lisatud logo ja tausta värv vahetus

--- components/pages/home.tsx
+++ components/pages/home.tsx
@@ -1,12 +1,23 @@
-
+import Image from "next/image";
 import MenuButton from "@/components/buttons/menu-button";
 
 export default async function HomePage() {
   return (
     <>
-      <div className="flex flex-col items-center justify-start min-h-screen bg-green-900">
-        <h1 className="text-8xl font-bold mt-40 whitespace-nowrap">Perudo Palace</h1>
-        <div className="grid grid-cols-2 gap-6 mt-32">
+      <div className="flex flex-col items-center justify-start min-h-screen bg-gray-800">
+        {/* Logo */}
+        <div className="mt-20">
+        <Image
+        src="/image/Logo.png" // Määrake kaust ja failinimi
+        alt="Perudo Palace Logo"
+        width={300} // Kohanda vastavalt vajadusele
+        height={100} // Kohanda vastavalt vajadusele
+        priority
+        />
+        </div>
+
+        {/* Menüünupud */}
+        <div className="grid grid-cols-2 gap-6 mt-12">
           <MenuButton title="Sign In" href="/sign-in" />
           <MenuButton title="Sign Up" href="/sign-up" />
           <MenuButton title="Join Game" href="/join" />
