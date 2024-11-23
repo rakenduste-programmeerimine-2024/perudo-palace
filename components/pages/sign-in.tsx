06521ef4cf6b60729cd337d6edc8e1dea950ac2e
Joosep Madar <146342736+joosep5@users.noreply.github.com>
2024-11-23 12:59:43 +0200

    add new colors
    lisatud uued värvid

--- components/pages/sign-in.tsx
+++ components/pages/sign-in.tsx
@@ -8,35 +8,45 @@ import BackButton from "@/components/buttons/back-button";
 export default async function LoginPage(props: { searchParams: Promise<Message> }) {
   const searchParams = await props.searchParams;
   return (
-    <>
-    <div className="flex flex-col items-center justify-start">
-      <h1 className="text-8xl font-bold mt-40 whitespace-nowrap">Perudo Palace</h1>
-    </div>
-    <form className="flex flex-col items-center justify-start mt-20">
-      <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-        <Label htmlFor="email">Email</Label>
-        <Input name="email" placeholder="you@example.com" required />
-        <div className="flex justify-between items-center">
-          <Label htmlFor="password">Password</Label>
-        </div>
-        <Input
-          type="password"
-          name="password"
-          placeholder="Your password"
-          required
-        />
-        <SubmitButton 
-          className="bg-gray-500 hover:bg-gray-600 text-white shadow-lg" 
-          pendingText="Signing In..." 
-          formAction={signInAction}>
-          Sign in
-        </SubmitButton>
-        <FormMessage message={searchParams} />
+    <div className="min-h-screen w-screen bg-gray-800 flex flex-col items-center justify-center">
+      {/* Pealkiri */}
+      <div className="flex flex-col items-center justify-start">
+        <h1 className="text-8xl font-bold mt-0 text-orange-500 whitespace-nowrap">
+          Perudo Palace
+        </h1>
       </div>
-    </form>
-    <div className="absolute bottom-0 left-0 mb-4 ml-4 py-2 px-4 rounded">
-      <BackButton href="/"/>
+      {/* Vorm */}
+      <form className="flex flex-col items-center justify-start mt-20 w-full max-w-md space-y-4 bg-gray-900 p-6 rounded-md shadow-lg">
+        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+          <Label htmlFor="email" className="text-orange-500">
+            Email
+          </Label>
+          <Input name="email" placeholder="you@example.com" required />
+          <div className="flex justify-between items-center">
+            <Label htmlFor="password" className="text-orange-500">
+              Password
+            </Label>
+          </div>
+          <Input
+            type="password"
+            name="password"
+            placeholder="Your password"
+            required
+          />
+          <SubmitButton
+            className="bg-orange-500 hover:bg-orange-600 text-white shadow-lg"
+            pendingText="Signing In..."
+            formAction={signInAction}
+          >
+            Sign in
+          </SubmitButton>
+          <FormMessage message={searchParams} />
+        </div>
+      </form>
+      {/* Tagasi nupp */}
+      <div className="absolute bottom-0 left-0 mb-4 ml-4 py-2 px-4 rounded">
+        <BackButton href="/" />
+      </div>
     </div>
-    </>
   );
 }
